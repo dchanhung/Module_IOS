fc76470b2aa99053b78f623d2bfe482f5ccdb4ba
Hung Dep Trai <139764345+dchanhung@users.noreply.github.com>
2025-06-14 00:07:14 +0700

    Update and rename Locket_DuyVinh09.js to Locket_dchanhung.js

--- js/Locket_dchanhung.js
+++ js/Locket_dchanhung.js
@@ -8,7 +8,7 @@ const mapping = {
 var ua = $request.headers["User-Agent"] || $request.headers["user-agent"],
   obj = JSON.parse($response.body);
 obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
-var duyvinh09 = {
+var dchanhung = {
       is_sandbox: !1,
       ownership_type: "PURCHASED",
       billing_issues_detected_at: null,
@@ -16,21 +16,21 @@ var duyvinh09 = {
       expires_date: "2099-01-09T10:10:14Z",
       grace_period_expires_date: null,
       unsubscribe_detected_at: null,
-      original_purchase_date: "2005-01-09T10:10:15Z",
-      purchase_date: "2005-01-09T10:10:14Z",
+      original_purchase_date: "2007-09-14T10:10:15Z",
+      purchase_date: "2007-09-14T10:10:14Z",
       store: "app_store"
   },
   locketGold = {
       grace_period_expires_date: null,
-      purchase_date: "2005-01-09T10:10:14Z",
-      product_identifier: "com.duyvinh09.premium.yearly",
+      purchase_date: "2005-09-14T10:10:14Z",
+      product_identifier: "com.dchanhung.premium.yearly",
       expires_date: "2099-01-09T10:10:14Z"
   };
 const match = Object.keys(mapping).find(e => ua.includes(e));
 if (match) {
   let [e, s] = mapping[match];
-  s ? (locketGold.product_identifier = s, obj.subscriber.subscriptions[s] = duyvinh09) : obj.subscriber.subscriptions["com.duyvinh09.premium.yearly"] = duyvinh09, obj.subscriber.entitlements[e] = locketGold
-} else obj.subscriber.subscriptions["com.duyvinh09.premium.yearly"] = duyvinh09, obj.subscriber.entitlements.pro = locketGold;
+  s ? (locketGold.product_identifier = s, obj.subscriber.subscriptions[s] = dchanhung) : obj.subscriber.subscriptions["com.dchanhung.premium.yearly"] = dchanhung, obj.subscriber.entitlements[e] = locketGold
+} else obj.subscriber.subscriptions["com.dchanhung.premium.yearly"] = dchanhung, obj.subscriber.entitlements.pro = locketGold;
 $done({
   body: JSON.stringify(obj)
 });
